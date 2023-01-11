d1facd631503005ae136ec2331b1ee0cefcaee9e
muxutdinov <jmuxutdinov@mail.ru>
2023-01-11 11:14:54 +0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React, { useEffect } from 'react'
-import { Route,Routes,Navigate,redirect} from 'react-router-dom'
+import { Route,Routes,useNavigate} from 'react-router-dom'
 import Navbar from './components/Navbar'
 import './App.css'
+import MatchesPage from './pages/MatchesPage'
 
 const App = () => {
-
+  const navigate = useNavigate();
+  useEffect(()=>{
+    navigate('/home')
+  },[])
   return (
     <div>
       <Navbar/>
       <Routes>
-        <Route path='/' element={<h1>Home</h1>}/>
+        <Route path='/home' element={<h1>Home</h1>}/>
+        <Route path='/matches' element={<MatchesPage/>}/>
       </Routes>
     </div>
   )
